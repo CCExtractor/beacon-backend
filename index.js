db92c8db5401491e64fa2201ec1feb132915cd00
Aadi Bajpai <hello@aadibajpai.com>
2021-06-13 20:25:55 -0500

    lint

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs } = require('./schema')
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs } = require("./schema");
 
 async function startApolloServer() {
-  // Provide resolver functions for your schema fields
-  const resolvers = {
-    Query: {
-      hello: () => 'Hello world!',
-    },
-  };
+    // Provide resolver functions for your schema fields
+    const resolvers = {
+        Query: {
+            hello: () => "Hello world!",
+        },
+    };
 
-  const server = new ApolloServer({ typeDefs, resolvers });
-  await server.start();
+    const server = new ApolloServer({ typeDefs, resolvers });
+    await server.start();
 
-  const app = express();
-  server.applyMiddleware({ app });
+    const app = express();
+    server.applyMiddleware({ app });
 
-  await new Promise(resolve => app.listen({ port: 4000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
-  return { server, app };
+    await new Promise(resolve => app.listen({ port: 4000 }, resolve));
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+    return { server, app };
 }
 
 (async () => startApolloServer())();
