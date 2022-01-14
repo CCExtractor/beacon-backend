aa7be2358942c245eca73d17b92b0c8461e24698
Aadeesh11 <asoswal@gmail.com>
2022-01-15 01:35:02 +0530

    rfrac: ran prettier.

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -117,14 +117,14 @@ const resolvers = {
             const beacon = await Beacon.findById(beaconID).populate("leader");
 
             if (!beacon) return new UserInputError("No beacon exists with that id.");
-            if (beacon.leader.id != user.id) return new Error("Only the leader is allowed to change the beacon duration.");
-            if (beacon.startsAt.getTime() > newExpiresAt) return Error("Beacon can not expire before it has started.")
+            if (beacon.leader.id != user.id)
+                return new Error("Only the leader is allowed to change the beacon duration.");
+            if (beacon.startsAt.getTime() > newExpiresAt) return Error("Beacon can not expire before it has started.");
 
             beacon.expiresAt = newExpiresAt;
             await beacon.save();
 
             return beacon;
-
         },
 
         joinBeacon: async (_, { shortcode }, { user, pubsub }) => {
