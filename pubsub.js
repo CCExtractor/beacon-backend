4c6dc7faade80cc5495969ca9a4fdc50b3c834da
Shock@5678 <abhishekbw@foyer.work>
2024-09-02 01:44:34 +0530

    prettier applied

--- pubsub.js
+++ pubsub.js
@@ -1,18 +1,18 @@
-const { RedisPubSub } = require("graphql-redis-subscriptions");
-const Redis = require("ioredis");
+// const { RedisPubSub } = require("graphql-redis-subscriptions");
+// const Redis = require("ioredis");
 
-const options = {
-    host: process.env.REDIS_URL,
-    port: process.env.REDIS_PORT,
-    password: process.env.REDIS_AUTH,
-    username: process.env.REDIS_USERNAME,
-    retryStrategy: times => {
-        // reconnect after
-        return Math.min(times * 50, 2000);
-    },
-};
+// const options = {
+//     host: process.env.REDIS_URL,
+//     port: process.env.REDIS_PORT,
+//     password: process.env.REDIS_AUTH,
+//     username: process.env.REDIS_USERNAME,
+//     retryStrategy: times => {
+//         // reconnect after
+//         return Math.min(times * 50, 2000);
+//     },
+// };
 
-module.exports = new RedisPubSub({
-    publisher: new Redis(options),
-    subscriber: new Redis(options),
-});
+// module.exports = new RedisPubSub({
+//     publisher: new Redis(options),
+//     subscriber: new Redis(options),
+// });
