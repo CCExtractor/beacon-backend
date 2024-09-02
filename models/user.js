c008146a0d464e37f4b8176980bb0e0c1e8a0f41
Shock@5678 <abhishekbw@foyer.work>
2024-09-02 08:39:23 +0530

    changing comment line

--- models/user.js
+++ models/user.js
@@ -9,8 +9,8 @@ const UserSchema = new Schema(
         name: String,
         email: String,
         password: String,
-        // store most recent location
         isVerified: { type: Boolean, default: false },
+        // store most recent location
         location: LocationSchema,
         beacons: { type: [Schema.Types.ObjectId], ref: "Beacon", default: [] },
         groups: { type: [Schema.Types.ObjectId], ref: "Group", default: [] },
